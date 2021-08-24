626ec109c606c920d19299a13d653ced25494c35
Mike Codeur <cabanas.mickael@gmail.com>
2021-08-24 20:22:09 +0800

    a

---
+++ src/final/04.js
@@ -0,0 +1,88 @@
+// Composants composés avec Context
+// http://localhost:3000/alone/final/02.js
+
+import * as React from 'react'
+import CheckBox from '../checkbox'
+
+const callAll = (...fns) => (...args) => fns.forEach(fn => fn?.(...args))
+
+function toggleReducer(state, {type, initialState}) {
+  switch (type) {
+    case 'toggle': {
+      return {on: !state.on}
+    }
+    case 'reset': {
+      return initialState
+    }
+    default: {
+      throw new Error(`Unsupported type: ${type}`)
+    }
+  }
+}
+
+function useCheckBox({initialOn = false, reducer = toggleReducer} = {}) {
+  const {current: initialState} = React.useRef({on: initialOn})
+  const [state, dispatch] = React.useReducer(reducer, initialState)
+  const {on} = state
+
+  const toggle = () => dispatch({type: 'toggle'})
+  const reset = () => dispatch({type: 'reset', initialState})
+
+  function getTogglerProps({onClick, ...props} = {}) {
+    return {
+      'aria-pressed': on,
+      onClick: callAll(onClick, toggle),
+      ...props,
+    }
+  }
+
+  function getResetterProps({onClick, ...props} = {}) {
+    return {
+      onClick: callAll(onClick, reset),
+      ...props,
+    }
+  }
+
+  return {
+    on,
+    reset,
+    toggle,
+    getTogglerProps,
+    getResetterProps,
+  }
+}
+
+function App() {
+  const [timesClicked, setTimesClicked] = React.useState(0)
+  const clickedTooMuch = timesClicked >= 4
+
+  function toggleStateReducer(state, action) {
+    switch (action.type) {
+      case 'toggle': {
+        if (clickedTooMuch) {
+          return {on: state.on}
+        }
+        return {on: !state.on}
+      }
+      case 'reset': {
+        return {on: false}
+      }
+      default: {
+        throw new Error(`Unsupported type: ${action.type}`)
+      }
+    }
+  }
+
+  const {on, getTogglerProps, getResetterProps} = useCheckBox({
+    reducer: toggleStateReducer,
+  })
+  return (
+    <div>
+      {/* <CheckBox checked={checked}  {...checkboxProps} /> */}
+      <br />
+     
+    </div>
+  )
+}
+
+export default App
